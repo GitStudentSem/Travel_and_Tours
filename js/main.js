7a6806b34de6b8b2fcd7bda5ff251544dd48ee74
GitStudentSem <programmSem@yandex.ru>
2021-01-08 12:04:03 +0300

    minor fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 var burger = document.querySelector(".header__burger");
 burger.addEventListener("click", function () {
-  console.log("Клик");
   document
     .querySelector(".header__menu")
     .classList.toggle("header__menu-visible");
