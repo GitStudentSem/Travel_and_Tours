63030fb1c72b3631e1b3c07135676c83b21e7d9c
GitStudentSem <programmSem@yandex.ru>
2021-01-09 11:27:17 +0300

    fix modal dialog

--- js/main.js
+++ js/main.js
@@ -1,6 +1,37 @@
-var burger = document.querySelector(".header__burger");
-burger.addEventListener("click", function () {
-  document
-    .querySelector(".header__menu")
-    .classList.toggle("header__menu-visible");
+$(document).ready(function () {
+  var burger = document.querySelector(".header__burger");
+  burger.addEventListener("click", function () {
+    document
+      .querySelector(".header__menu")
+      .classList.toggle("header__menu-visible");
+  });
+
+  var modalButton = $("[data-toggle='modal']");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  // Нажатие Escape
+  $(document).keyup(function (e) {
+    if (e.key === "Escape" || e.keyCode === 27) {
+      var modalOverlay = $(".modal__overlay");
+      var modalDialog = $(".modal__dialog");
+      modalOverlay.removeClass("modal__overlay-visible");
+      modalDialog.removeClass("modal__dialog-visible");
+    }
+  });
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay-visible");
+    modalDialog.addClass("modal__dialog-visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay-visible");
+    modalDialog.removeClass("modal__dialog-visible");
+  }
 });
