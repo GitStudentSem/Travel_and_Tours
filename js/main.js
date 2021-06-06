96a3f509535cbde10406eea1e5826284d3db90ab
GitStudentSem <programmSem@yandex.ru>
2021-06-06 21:41:24 +0300

    fix js and minir fix

--- js/main.js
+++ js/main.js
@@ -1,58 +1,54 @@
-$(document).ready(function () {
-  var burger = document.querySelector(".header__burger");
-  burger.addEventListener("click", function () {
-    document
-      .querySelector(".header__menu")
-      .classList.toggle("header__menu-visible");
-  });
+window.onload = function () {
+  "use strict";
+  // Блокировка скролла
+  const disableScrolling = () => {
+    let x = window.scrollX;
+    let y = window.scrollY;
+    window.onscroll = () => {
+      window.scrollTo(x, y);
+    };
+  };
+  // Модальное окно
+  const modal = () => {
+    const body = document.querySelector("body");
+    const modal = document.querySelector(".modal");
 
-  var modalButton = $("[data-toggle='modal']");
-  var closeModalButton = $(".modal__close");
-  modalButton.on("click", openModal);
-  closeModalButton.on("click", closeModal);
+    body.addEventListener("click", (event) => {
+      let target = event.target;
 
-  // Нажатие Escape
-  $(document).keyup(function (e) {
-    if (e.key === "Escape" || e.keyCode === 27) {
-      var modalOverlay = $(".modal__overlay");
-      var modalDialog = $(".modal__dialog");
-      modalOverlay.removeClass("modal__overlay-visible");
-      modalDialog.removeClass("modal__dialog-visible");
-    }
-  });
+      if (target.closest("[data-toggle='modal']")) {
+        event.preventDefault();
+        modal.classList.add("active");
+        disableScrolling();
+        // Нажатие на крестик
+      } else if (target.matches(".modal__window-close")) {
+        event.preventDefault();
+        modal.classList.remove("active");
+        window.onscroll = () => {};
+      } else if (target.matches(".active")) {
+        event.preventDefault();
+        modal.classList.remove("active");
+        window.onscroll = () => {};
+      }
+    });
+  };
+  modal();
 
-  function openModal() {
-    var modalOverlay = $(".modal__overlay");
-    var modalDialog = $(".modal__dialog");
-    modalOverlay.addClass("modal__overlay-visible");
-    modalDialog.addClass("modal__dialog-visible");
-  }
-  function closeModal(event) {
-    event.preventDefault();
-    var modalOverlay = $(".modal__overlay");
-    var modalDialog = $(".modal__dialog");
-    modalOverlay.removeClass("modal__overlay-visible");
-    modalDialog.removeClass("modal__dialog-visible");
-  }
-  // Swiper
-  var mySwiper = new Swiper(".swiper-container", {
-    // Optional parameters
-    loop: true,
+  // Мобильное меню
+  const mobileMenu = () => {
+    const burger = document.querySelector(".header__burger");
+    const menu = document.querySelector(".header__menu");
 
-    // If we need pagination
-    pagination: {
-      el: ".swiper-pagination",
-    },
+    burger.addEventListener("click", () => {
+      menu.classList.toggle("header__menu-visible");
 
-    // Navigation arrows
-    navigation: {
-      nextEl: ".slider__button-next",
-      prevEl: ".slider__button-prev",
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-      el: ".swiper-scrollbar",
-    },
-  });
-});
+      if (menu.classList.contains("header__menu-visible")) {
+        console.log(1);
+        disableScrolling();
+      } else {
+        window.onscroll = () => {};
+      }
+    });
+  };
+  mobileMenu();
+};
